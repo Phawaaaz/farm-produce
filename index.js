256e0fe2056bbbb6f565aaf2a2e02bed4f7c7f7c
Phawaaz <phawaazakin@gmail.com>
2025-04-20 20:52:21 +0100

    everything the light touch is part of our kingdom

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const http = require('http');
 const { json } = require('stream/consumers');
 const url = require('url')
 
+const slugify = require('slugify')
+
 const replaceTemplate = require('./modules.js/replaceTemplate')
 
 // files
@@ -50,8 +52,11 @@ const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'u
 const tempProduct  = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8')
  
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
-const dataObj = JSON.parse(data)
-     
+const dataObj = JSON.parse(data) 
+    
+
+const slug = dataObj.map(el => slugify(el.productName, {lower: true} ))
+console.log(slug);
 const server = http.createServer((req, res)=>{
 
   const { query, pathname } = url.parse(req.url, true);
